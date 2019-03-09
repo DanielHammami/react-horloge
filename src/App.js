2ba00ce28d1790cb8cc0e01c7decfe49528cae86
DanielHammami <daniel.hammami@yahoo.com>
2019-03-09 11:55:45 +0100

    Header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import './scss/styles.scss'
 
-class App extends Component {
+export default class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      title: 'React Clock'
+    };
+  } 
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header title={this.state.title}/>
       </div>
     );
   }
 }
-
-export default App;
